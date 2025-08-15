111b0113db4dff72d702319f9055fd2b387d8e2d
svl <4svl+git@pm.me>
2025-08-15 22:16:39 +0200

    .

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,12 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tsconfigPaths()],
   css: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
+    postcss: "./postcss.config.cjs",
   },
   server: {
     port: Number(process.env.VITE_PORT) || 5173,
